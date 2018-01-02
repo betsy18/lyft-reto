714428c8a7437cc1b4d4678608eadab74a91b34d
betsy18 <betsy_dessire@hotmail.com>
2018-01-02 03:39:34 -0500

    Implementando login.js

--- js/exit.js
+++ js/exit.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    
+  alert('Registro exitoso');
+  setTimeout(function() {
+    window.location.href = '../views/register.html';
+  }, 5000);
 });
--- js/login.js
+++ js/login.js
@@ -1,3 +1,70 @@
 $(document).ready(function() {
-   
+  // variables
+  var $txtName = $('#txtName');
+  var $txtLastName = $('#txtLastName');
+  var $txtEmail = $('#txtEmail');
+  var $chkBox = $('#chkBox');
+  var $btnNext = $('#btnNext');
+  var $validateName = false;
+  var $validateLastName = false;
+  var $validateEmail = false;
+  var $validateChk = false;
+  $btnNext.attr('disabled', true);
+
+  // eventos
+  $txtName.on('input', function(event) {
+    event.preventDefault();
+    if ($(this).val().length > 2) {
+      $validateName = true;
+      active();
+    } else {
+      $validateName = false;
+      $btnNext.attr('disabled', true);
+    }
   });
+
+  $txtLastName.on('input', function(event) {
+    event.preventDefault();
+    if ($(this).val().length > 2) {
+      $validateLastName = true;
+      active();
+    } else {
+      $validateLastName = false;
+      $btnNext.attr('disabled', true);
+    }
+  });
+
+  $txtEmail.on('input', function(event) {
+    event.preventDefault();
+    var regExpre = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    if (regExpre.test($(this).val())) {
+      $validateEmail = true;
+      active();
+    } else {
+      $validateEmail = false;
+      $btnNext.attr('disabled', true);
+    }
+  });
+
+  $chkBox.on('click', function(event) {
+    if (event.target.checked) {
+      $validateChk = true;
+      active();
+    } else {
+      $validateChk = false;
+      $btnNext.attr('disabled', true);
+    }
+  });
+
+  $btnNext.on('click', function(event) {
+    event.preventDefault();
+    window.location.href = '../views/exit.html';
+  });
+
+  // funciones para la validacion
+  function active() {
+    if ($validateName && $validateLastName && $validateEmail && $validateChk) {
+      $btnNext.attr('disabled', false);
+    }
+  }
+});
